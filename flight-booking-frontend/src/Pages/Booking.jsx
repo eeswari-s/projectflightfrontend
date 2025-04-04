4f9eb755b5f9031e9c54ae81ff7c8ecb0e865f05
eeswari-s <eeswarivaithee@gmail.com>
2025-04-04 23:44:01 +0530

    Remove unused payment API module and clean up Booking component

--- flight-booking-frontend/src/Pages/Booking.jsx
+++ flight-booking-frontend/src/Pages/Booking.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import SeatSelection from "../Components/SeatSelection";
-import { createCheckoutSession } from "../api/payApi.js";
+
 
 const BookingPage = () => {
   const location = useLocation();
